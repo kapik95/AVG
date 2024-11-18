362133d9919f95938a7825ba5e13c8c3c7324f20
Андрей Гомонец <andrei.gomonec@gmail.com>
2024-11-18 20:00:46 +0500

    Корректировка модели

--- Models/ProjectModel.cs
+++ Models/ProjectModel.cs
@@ -8,9 +8,9 @@ namespace AVG.Models
         public int Id { get; set; }
         
         [Required]
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Technologies { get; set; }
-        public string ImagePath { get; set;}
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Technologies { get; set; } = string.Empty;
+        public string ImagePath { get; set;} = string.Empty;
     }
 }
