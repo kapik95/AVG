90f088d0433f1d0f4caee3484f9299a23f426488
Andrei LAPTOP-OB0UMING <andrei.gomonec@gmail.com>
2024-11-29 14:22:45 +0500

    Рефакторинг. Убрал дубляж кода применив partial view

---
+++ Views/Home/ProjectCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AVG.Views.Home
+{
+    public class ProjectCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
